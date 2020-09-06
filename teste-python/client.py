554edb268f4a5d85b3462e0c788743eef571170a
huine <gabrielgisoldi@gmail.com>
2020-09-06 01:39:28 -0300

    Update network.py

--- teste-python/client.py
+++ teste-python/client.py
@@ -13,51 +13,59 @@ def chunk(it, size):
 class client(ClientTCP):
     """."""
 
-    def __init__(self, lista, id):
+    def __init__(self, lista):
         """."""
         self.lista = lista
-        super().__init__(id)
+        super().__init__()
 
-    def inicia(self, results):
+    def inicia(self):
         """."""
         super().connect()
         super().send(self.lista)
         response = super().receive()
-        for i in range(len(response)):
-            results[i] = response[i]
+        return response
+
+    def parse_multiprocessing(self, results):
+        """."""
+        response = self.inicia()
+        for i in range(len(response[0])):
+            results[i] = response[0][i]
 
 
 if __name__ == "__main__":
-    tmp = []
-    for j in range(30):
-        numeros = list(range(1, (140 * 71) + 1))
-        shuffle(numeros)
-        clients = []
-        jobs = []
-        output = []
+    # c = client(lista={})
+    # print(c.inicia())
+    # tmp = []
+    # for j in range(30):
+    # numeros = list(range(1, (140 * 71) + 1))
+    numeros = list(range(500000))
+    shuffle(numeros)
+    clients = []
+    jobs = []
+    output = []
 
-        for i in list(chunk(numeros, (len(numeros)//cpu_count() + 1))):
-            with open('/dev/random', 'rb') as f:
-                id_client = f.read(10).hex()
-            clients.append(
-                (
-                    client(lista=i, id=id_client),
-                    Array('Q', len(i))
-                )
+    for i in list(chunk(numeros, (len(numeros)//cpu_count() + 1))):
+        clients.append(
+            (
+                client(lista=i),
+                Array('Q', len(i))
             )
+        )
 
-        start = default_timer()
-        for item in clients:
-            P = Process(target=item[0].inicia, kwargs={'results': item[1]})
-            jobs.append(P)
-            P.start()
+    # start = default_timer()
+    for item in clients:
+        P = Process(target=item[0].parse_multiprocessing,
+                    kwargs={'results': item[1]})
+        jobs.append(P)
+        P.start()
 
-        for proc in jobs:
-            proc.join()
+    for proc in jobs:
+        proc.join()
 
-        for item in clients:
-            output.extend([i for i in item[1] if i > 0])
-        output.sort(reverse=True)
-        tmp.append(default_timer() - start)
+    for item in clients:
+        output.extend([i for i in item[1] if i > 0])
+    output.sort(reverse=True)
+    print(output[:222])
+    # tmp.append(default_timer() - start)
 
-    print('%.5f ms' % (1000 * (sum(tmp) / 30.0)))
+    # print('%.5f ms' % (1000 * (sum(tmp) / 30.0)))
--- teste-python/server.py
+++ teste-python/server.py
@@ -1,21 +1,8 @@
 from network import ServerTCP
-from ehPrimo import alg3
-
-def buscaPrimos(lista):
-    """."""
-    array = []
-    for item in lista:
-        if alg3(item) == 1:
-            array.append(item)
-    return array
-
-
-def teste(item):
-    item['response-teste'] = 20
-    return item
+from functions import function as f
 
 
 if __name__ == "__main__":
-    server = ServerTCP()
-    server.function = buscaPrimos
+    server = ServerTCP(timeout=15)
+    server.set_function((f.buscaPrimos,))
     server.start()
