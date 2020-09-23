22e5bb18d484f7f0bba9b4cfb9dd055dd9f6b9fa
huine <gabrielgisoldi@gmail.com>
2020-09-22 21:52:12 -0300

    Ajuste para nao ficar preciso no get da fila

--- aula6/barbeiro.py
+++ aula6/barbeiro.py
@@ -3,6 +3,8 @@ from time import sleep
 from queue import Queue, Empty, Full
 import random
 from timeit import default_timer
+from os import getpid
+from psutil import Process
 
 
 class Barbeiro(Thread):
@@ -56,7 +58,7 @@ if __name__ == "__main__":
     barbeiro = Barbeiro(nome="Barbeiro", cadeiras=5)
     barbeiro.running = True
     barbeiro.start()
-    for i in range(50):
+    for i in range(10):
         cliente = Cliente(nome=str(i), barbeiro=barbeiro)
         cliente.start()
         # Gera um cliente a cada 3 segundo(Stand in para 30 minutos)
@@ -72,3 +74,7 @@ if __name__ == "__main__":
     print('clientes_atendidos: %s' % clientes_atendidos)
     print('clientes_recusados: %s' % clientes_recusados)
     print('barbeiro dormindo: %.5f s' % barbeiro.tempo_dormindo)
+
+    pid = getpid()
+    proc = Process(pid)
+    proc.terminate()
